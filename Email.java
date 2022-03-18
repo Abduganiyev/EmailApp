2ccca164bff4414d084212154aaed43811067379
Abduganiyev <36122312+Abduganiyev@users.noreply.github.com>
2022-03-18 17:48:06 +0500

    Add files via upload

---
+++ Email.java
@@ -0,0 +1,66 @@
+public class Email {
+    private String title;
+    private String body;
+    private User sender;
+    private User receiver;
+    private Integer messageId;
+
+    public Integer getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Integer messageId) {
+        this.messageId = messageId;
+    }
+
+    private boolean statuts;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public boolean isStatuts() {
+        return statuts;
+    }
+
+    public void setStatuts(boolean statuts) {
+        this.statuts = statuts;
+    }
+
+    public Email(String title, String body, User sender, User receiver,Integer messageId, boolean statuts) {
+        this.title = title;
+        this.body = body;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageId=messageId;
+        this.statuts = statuts;
+    }
+}
